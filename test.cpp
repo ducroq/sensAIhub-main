b8ab7af3b5a23ddf72217772adabb5ca5a987217
jeroen_veen <jveen1@gmail.com>
2024-06-28 15:32:22 +0200

    Test files added

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::cout << "Hello from the Raspberry Pi!" << std::endl;
+    return 0;
+}
